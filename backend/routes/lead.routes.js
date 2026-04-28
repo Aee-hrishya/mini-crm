b90a867d3b4349ddc7bdd96acd9382bbc69dce16
Aee-hrishya <hshinde166@gmail.com>
2026-04-28 19:59:08 +0530

    dashboard controller and lead route

---
+++ backend/routes/lead.routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {
+    createLead,
+    getLeads,
+    getLeadById,
+    updateLead,
+    updateStatus
+} = require('../controllers/lead.controller');
+const {
+    addNote,
+    getNotes
+} = require("../controllers/note.controller");
+
+//leads
+router.post("/", createLead);
+router.get("/", getLeads);
+router.post("/:id", updateLead);
+router.get("/:id", getLeadById);
+router.post("/:id/status", updateStatus);
+
+//notes
+router.post("/:id/notes", addNote);
+router.get("/:id/notes", getNotes);
+
+module.exports = router;
